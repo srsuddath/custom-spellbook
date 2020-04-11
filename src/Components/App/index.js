67b418f9fda401a7b2821288e609e3a91544afe5
Sam <srsuddath@gmail.com>
2020-04-11 13:10:06 -0400

    add all the things

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
+
 import './styles.css';
 import Login from '../Login';
-
-
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loggedIn: false,
+      savedUsers: ['admin'],
+      savedPasswords: ['password'],
     };
-  };
+  }
 
-  componenetDidMount() {
+  componenetDidMount() {}
 
-  };
+  componenetWillUnmount() {}
 
-  componenetWillUnmount() {
-
+  onLoginUpdate = (loggedIn) => {
+    this.setState({ loggedIn });
   };
 
   render() {
+    const { savedUsers, savedPasswords, loggedIn } = this.state;
 
     return (
-      <div className='app' >
-        <Login />
+      <div className="app">
+        {!loggedIn && <Login onLoginUpdate={this.onLoginUpdate} usernames={savedUsers} passwords={savedPasswords} />}
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -2,39 +2,92 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 import './styles.css';
+import App from '../App';
 
 class Login extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       loggedIn: false,
+      usernames: props.usernames,
+      passwords: props.passwords,
+      usernameInput: '',
+      passwordInput: '',
+      message: '',
     };
+  }
+
+  checkCredentials = () => {
+    const { usernames, usernameInput, passwords, passwordInput } = this.state;
+    const index = usernames.indexOf(usernameInput);
+    if (index === -1) {
+      this.setState({ message: 'Username not found, please Register' });
+      return;
+    }
+    if (passwords[index] !== passwordInput) {
+      this.setState({ message: 'Incorrect password, please try again' });
+    }
+    this.setState({ message: '' });
+    this.props.onLoginUpdate(true);
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ passwordInput: event.target.value });
+  };
+
+  onUsernameChange = (event) => {
+    this.setState({ usernameInput: event.target.value });
   };
 
   render() {
+    const { message } = this.state;
 
     return (
       <div className="background">
         <div className="welcome">
           <h1>Custom Spellbook Generator</h1>
         </div>
+        {message && <h3>{message}</h3>}
         <div className="login-window">
           <h2>Please Log In</h2>
-          <input type="text" className="input-box" placeholder="Username" />
-          <input type="text" className="input-box" placeholder="Password" />
-          <button>Sign In</button>
-          <hr />
-          <button>Register</button>
-          <h4>or</h4>
-          <button>Forgot Your Password?</button>
-          <br />
 
+          <input
+            type="text"
+            className="input-box"
+            placeholder="Username"
+            value={this.state.usernameInput}
+            onChange={this.onUsernameChange}
+          />
+
+          <input
+            type="text"
+            className="input-box"
+            placeholder="Password"
+            value={this.state.passwordInput}
+            onChange={this.onPasswordChange}
+          />
+          <button onClick={this.checkCredentials}>Sign In</button>
+
+          <hr />
+
+          <button>Register</button>
+
+          <h4>or</h4>
+
+          <button>Forgot Your Password?</button>
+
+          <br />
         </div>
+
         <div className="spacer" />
       </div>
-    )
+    );
   }
 }
+
+Login.propTypes = {
+  usernames: PropTypes.arrayOf(PropTypes.string).isRequired,
+  passwords: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Login;
